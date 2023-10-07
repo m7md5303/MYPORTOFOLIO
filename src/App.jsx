785dcbdeaca0a831785e87ac133d0794d7d86fae
M7MD5303 <m7md5303@gmail.com>
2023-10-07 04:30:49 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import About from './Components/About';
 import Error from './Components/Error';
 function App() {
   const [show,setShow]=useState(true); 
-  const alv=()=>{{setShow(false);alert("For proper view rotate your phone to landscape view🔄 however, PC 💻would be better😃");  }}
+  const alv=()=>{{setShow(false);alert("For proper view rotate your phone to landscape view🔄 however, PC 💻would be better😃.");  }}
   return (
     <div className='main' onLoadCapture={()=>{if(show)alv();}}>
       <Router >
