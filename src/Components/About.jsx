8a94b51a0ee903a962ec3b5c8c7961f39c864d00
M7MD5303 <m7md5303@gmail.com>
2023-12-14 22:26:47 +0200

    m

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -105,7 +105,7 @@ export default function About() {
     <div className='cvtxt'>
 <h1>My C.V.</h1>
 <p>Eventually, you can easily view my updated C.V. from the link below.😃</p>
-<Link to="https://drive.google.com/file/d/1dhdN8A6Tl2UFPuaDSgc2AIMEO-wQ79I9/view?usp=sharing">M.K. C.V. </Link>
+<Link to="https://drive.google.com/drive/u/0/folders/1cTCipDT_ysKDqME_pj1byzpJLjbE2Juq">M.K. C.V. </Link>
     </div>
     <img src={cv}alt='Loading⏳'/>
 </div>
