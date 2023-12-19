041734c5bd75f3058c86274adcbe52546739913f
M7MD5303 <m7md5303@gmail.com>
2023-12-19 19:37:40 +0200

    fix

--- src/Components/Botnav.jsx
+++ src/Components/Botnav.jsx
@@ -11,7 +11,7 @@ export default function Botnav(props) {
         <div className='botnav'>
     
             <div className='botit'>
-            <Botnavitem tit1={"m7md5303@gmail.com"} img1={gm} sh={true}/>
+            <Botnavitem tit1={"E-mail"} img1={gm} sh={true}/>
             <Botnavitem tit2={"Facebook"} img2={fc} sh={false} linka={"https://www.facebook.com/Mohamed.khaled.5303/"}/>
             <Botnavitem tit2={"LinkedIn"} img2={ln} sh={false} linka={"https://www.linkedin.com/in/mohamed-khaled-51144a69/"}/>
             <Botnavitem tit2={"GitHub"} img2={gh} sh={false} linka={"https://github.com/m7md5303"}/>
