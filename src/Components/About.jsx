ceae20c0e421f4db5e7ba57aac2ec4099c8113c4
M7MD5303 <m7md5303@gmail.com>
2026-01-16 14:05:15 +0200

    img fix

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ export default function About() {
         </div>
         
        </div>
-<img src={expintro}alt='Loading⏳' style={{maxHeight:'75vh'}} />
+<img src={expintro}alt='Loading⏳' style={{maxHeight:'75vh'}} className='exp'/>
 
         </div>
         <div className='cont'>
